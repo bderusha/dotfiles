59169e5585eb5cf8a84ab765d3bdaa86c130a87e
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2022-12-13 16:17:48 -0500

    update flakes

--- home.nix
+++ home.nix
@@ -19,6 +19,11 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  # programs.bash = {
+  #   enable = true;
+  #   enableCompletion = true;
+  # };
+
   programs.git = {
     enable = true;
 
@@ -26,9 +31,68 @@
     userEmail = "bderush+123456@github.com";
     aliases = {
       sw = "switch";
-      tmp = "checkout -b tmp123";
+      tmp = "checkout -b tmp0123";
+      tmp1 = "checkout -b tmp123";
+      tmp2 = "checkout -b tmp321";
+      tmp3 = "checkout -b tmp246";
     };
   };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      format = "$username$hostname$directory$git_branch$git_commit$git_state$git_status$cmd_duration$line_break$jobs$character";
+      username = {
+        style = "bold blue";
+        disabled = true;
+      };
+      hostname = {
+        style = "bold blue";
+        disabled = true;
+      };
+      directory = {
+        style = "bold blue";
+        disabled = false;
+      };
+      git_branch = {
+        style = "bold blue";
+        disabled = false;
+      };
+      git_commit = {
+        style = "bold blue";
+        disabled = false;
+      };
+      git_state = {
+        style = "bold blue";
+        disabled = false;
+      };
+      git_status = {
+        style = "bold blue";
+        disabled = false;
+      };
+      cmd_duration = {
+        style = "bold blue";
+        disabled = false;
+      };
+      line_break = {
+        style = "bold blue";
+        disabled = false;
+      };
+      jobs = {
+        style = "bold blue";
+        disabled = false;
+      };
+      character = {
+        style = "bold blue";
+        disabled = false;
+      };
+    };
+  };
+
+  home.packages = with pkgs; [
+    cowsay
+  ];
 
   services.gpg-agent = {
     enable = true;
